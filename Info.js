f93fd6692962c376c289d7f42bf965f781d0c26d
Draigana <160855547+Draigana@users.noreply.github.com>
2024-12-11 16:12:28 +0100

    Create Info.js

---
+++ Info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component{
+ render(){
+ return(
+    <div>
+        <h2>Додаток про погоду</h2>
+        <p>Інформація</p>
+    </div>
+       );
+     }
+  }
+
+export default Info;
